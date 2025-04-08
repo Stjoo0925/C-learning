fbcf2c99fa3d8d939f2c5a00fb0c38e52bd5e163
Stjoo0925 <stjoo0925@gmail.com>
2025-04-08 22:18:07 +0900

    feat : Console 메서드 기능 설명 및 간단한 변수 초기화 방법

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// this is another comment line
+
+// this line of code will print "Hello, Denis!" to the console
 Console.WriteLine("Hello, Denis!");
+
+// declare a (String)variable
+string myFriendsName;
+// assign a value to the myFriendsName variable
+myFriendsName = "Jannick";
+
+// declare a variable and assign a value to it.
+string MyFriendsName2 = "Frank";
+
+// use/access the variable
+Console.WriteLine(myFriendsName);
+
+// overwriting the variable value
+myFriendsName = "Frank";
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prints out whatever is in side of ()
+Console.WriteLine("Enter something!");
+
+// takes the user input and stores it
+// Variable with the name "userInput" and the data type "string"
+// strings are used for text
+String userInput = Console.ReadLine();
+
+// we are adding to string togehter and writing them onto the console
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();
