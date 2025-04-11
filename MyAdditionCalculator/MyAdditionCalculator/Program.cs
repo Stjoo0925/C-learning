e30af0a46d16684d94a660ce18f8e3dfa93bc8f3
Stjoo0925 <stjoo0925@gmail.com>
2025-04-11 18:28:54 +0900

    feat : 더블을 사용하여 십진수 표현하기

--- MyAdditionCalculator/MyAdditionCalculator/Program.cs
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -1,27 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
 // datatype variableName = initial value;
-int myNumber = 0;
-int myNumber2 = 0;
+double myNumber1 = 0.0;
+double myNumber2 = 0.0;
 
 // Prints out whatever is in side of ()
-Console.WriteLine("Enter a  whole number!");
-
+Console.WriteLine("Enter a number!");
 
 // takes the user input and stores it
 // Variable with the name "userInput" and the data type "string"
-// strings are used for text
-String userInput = Console.ReadLine();
-// myNumber = int.Parse(Console.ReadLine());
-myNumber = int.Parse(userInput);
+// strings are used for text 
+string userInput = Console.ReadLine();
+//myNumber = int.Parse(Console.ReadLine());
+myNumber1 = double.Parse(userInput);
 
-// Prints out whatever is in side of ()
-Console.WriteLine("Enter a  whole number!");
+Console.WriteLine("Enter a number!");
 userInput = Console.ReadLine();
-myNumber2 = int.Parse(userInput);
+myNumber2 = double.Parse(userInput);
 
-int sum = myNumber + myNumber2;
 
-// we are adding to string togehter and writing them onto the console
-Console.WriteLine("The result of " + myNumber + " and " + myNumber2 + " is " + sum);
+
+double sum = myNumber1 + myNumber2;
+
+// we are adding to strings together and writing them onto the console
+// string concatination
+//Console.WriteLine("The result of " + myNumber + " and " + myNumber2 + " is " + sum);
+
+// string interpolation
+Console.WriteLine($"The result of {myNumber1} and {myNumber2} is {sum}");
+
 Console.ReadKey();
